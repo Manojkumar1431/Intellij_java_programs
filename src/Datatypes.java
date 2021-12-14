0e240b596053845fddc4483ed01cf3fec079ae22
Manojkumar1431 <manojkumar.b1431@gmail.com>
2021-12-14 11:58:56 +0530

    commit

---
+++ src/Datatypes.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class Datatypes {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to my world");
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        String name =sc.next();
+
+        System.out.println("Hi "+name+" welcome to "+a );
+    }
+}
